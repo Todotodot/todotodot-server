94a32922e4e20aeb983e56f89728c4c5c153b087
eggfreitag <93723399+eggfreitag@users.noreply.github.com>
2022-06-05 23:34:52 +0900

    그룹 CRUD 및 Personal Todo 가져오기 구현 (#10)
    * chore: Implement group CRUD and api to read personal todos
    * refactor: Change router
    * refactor: refactor JSON format & status code
    
    Co-authored-by: Wonhee Kang

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const loader = require("./loader");
-
 const index = require("./routes/index");
 const user = require("./routes/user");
-// const group = require("./routes/group");
-
+const group = require("./routes/group");
 const { unknownPageHandler, errorHandler } = require("./middlewares/errorHandler");
 
 const app = express();
@@ -12,10 +10,9 @@ const app = express();
 (async () => {
   await loader(app);
 
-  // app.use(미들웨어); 미들웨어
   app.use("/", index);
   app.use("/users", user);
-  // app.use("/groups", group);
+  app.use("/groups", group);
 
   app.use(unknownPageHandler);
   app.use(errorHandler);
