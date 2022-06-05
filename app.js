b2f3ca915c6e3a545f38d90646e846bbcb238fd2
daeunKim <73865700+KimDaEun1031@users.noreply.github.com>
2022-06-05 14:57:39 +0900

    chore: Setup Packages (#4)
    Co-authored-by: daeunKim

--- app.js
+++ app.js
@@ -9,29 +9,31 @@ const cors = require("cors");
 
 const index = require("./routes/index");
 
-app.use(cors({
-  origin: "http://localhost:3000",
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", index);
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   res.status(err.status || 500);
   res.json({
-    "result": "error",
-    "error": {
-      "message": "Request Failed",
-      "status": 500,
-    }
+    result: "error",
+    error: {
+      message: "Request Failed",
+      status: 500,
+    },
   });
 });
 
