1e8c97c4501bc39ab0cc8e1ee4c401e576eec448
Dana <88199458+JihyunGong@users.noreply.github.com>
2022-06-06 19:18:39 +0900

    feat: Get group todos (#11)
    * feat: Update user todo
    
    * refactor: Refactor codes
    
    * feat: Get group todos
    
    * refactor: RefactortodoController.js
    
    Co-authored-by: jihyungong <jihyungong@JIHYUNui-MacBookPro.local>

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const User = require("../models/User");
 const Todo = require("../models/Todo");
 const catchAsync = require("../utils/catchAsync");
@@ -25,8 +26,18 @@ exports.createTodo = catchAsync(async (req, res, next) => {
 
   const { groupId } = req.params;
 
+  if (!mongoose.isValidObjectId(groupId)) {
+    return res.json({
+      result: "error",
+      error: {
+        message: "유효하지 않은 그룹입니다.",
+        status: 400,
+      },
+    });
+  }
+
   if (groupId) {
-    const group = user.group.findOne({ groupId });
+    const group = user.group.findById(groupId);
 
     group.todos.push(newTodo._id);
   } else {
@@ -55,7 +66,17 @@ exports.updateTodo = catchAsync(async (req, res, next) => {
 
   const { todoId } = req.params;
 
-  await Todo.findOneAndUpdate({ _id: todoId }, { title, content });
+  if (!mongoose.isValidObjectId(todoId)) {
+    return res.json({
+      result: "error",
+      error: {
+        message: "유효하지 않은 Todo입니다.",
+        status: 400,
+      },
+    });
+  }
+
+  await Todo.findByIdAndUpdate({ _id: todoId }, { title, content });
 
   return res.json({
     result: "success",
--- routes/group.js
+++ routes/group.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const isLoggedIn = require("../middlewares/authorization");
 const GroupController = require("../controllers/groupController");
+const TodoController = require("../controllers/todoController");
 
 const router = express.Router();
 
+router.get("/groups/:groupId/todos", isLoggedIn, GroupController.getGroupTodos);
 router.post("/", isLoggedIn, GroupController.createGroup);
 router.patch("/:groupId", isLoggedIn, GroupController.updateGroup);
 router.delete("/:groupId", isLoggedIn, GroupController.deleteGroup);
+
+router.post("/groups/:groupId/todos", isLoggedIn, TodoController.createTodo);
+router.patch("/groups/:groupId/todos/:todoId", isLoggedIn, TodoController.updateTodo);
 
 module.exports = router;
