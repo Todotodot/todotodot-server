36965682800ee8fb10eb0d6b7aa26ef4a8f6997f
Wonhee Kang <eggfreitag@MacMorning.local>
2022-06-06 21:22:10 +0900

    feat: Implement authorization middleware

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,5 +1,26 @@
-const isLoggedIn = (req, res, next) => {
-  next();
-};
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const catchAsync = require("../utils/catchAsync");
+
+const isLoggedIn = catchAsync(async (req, res, next) => {
+  const { profile } = req.localStorage;
+
+  if (profile) {
+    const userId = jwt.verify(profile, process.env.SECRET_KEY);
+    const user = await User.findById(userId).lean();
+
+    req.user = user;
+    next();
+  } else {
+    return res.json({
+      result: "error",
+      error: {
+        message: "유효하지 않은 접근입니다.",
+        status: 400,
+      },
+    });
+  }
+});
 
 module.exports = isLoggedIn;
