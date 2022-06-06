109dd5969debbffcdfc19a48ae676700d571b7ee
Wonhee Kang <eggfreitag@MacMorning.local>
2022-06-06 21:28:09 +0900

    refactor: Refactor isLoggedIn function

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
 
+const User = require("../models/User");
 const catchAsync = require("../utils/catchAsync");
 
 const isLoggedIn = catchAsync(async (req, res, next) => {
