ae12fd241d1a539b709b6ef2e5c74cf3077cb068
daeunKim <kimde1031@gmail.com>
2022-06-07 16:21:03 +0900

    refacotr: add catchAsync in Auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,44 +3,13 @@ const jwt = require("jsonwebtoken");
 const { firebaseAdminAuth } = require("../config/firebase");
 const { secretKey, option } = require("../config/secretkey");
 const User = require("../models/User");
+const catchAsync = require("../utils/catchAsync");
 
-exports.login = async (req, res) => {
-  try {
-    const idToken = req.headers.authorization.split(" ")[1];
-    const verifiedToken = await firebaseAdminAuth.verifyIdToken(idToken);
+exports.login = catchAsync(async (req, res) => {
+  const idToken = req.headers.authorization.split(" ")[1];
+  const verifiedToken = await firebaseAdminAuth.verifyIdToken(idToken);
 
-    if (!verifiedToken) {
-      res.json({
-        result: "error",
-        error: {
-          message: "Unauthorized",
-          status: 401,
-        },
-      });
-    }
-
-    const { name, email } = verifiedToken;
-    const user = await User.findOne({ email }).lean();
-    let id = user._id;
-
-    if (!user) {
-      const newUser = await User.create({
-        name,
-        email,
-      });
-
-      id = newUser._id;
-    }
-
-    const accessToken = jwt.sign({ id }, secretKey, option);
-
-    if (accessToken) {
-      res.json({
-        result: "success",
-        token: accessToken,
-      });
-    }
-  } catch (error) {
+  if (!verifiedToken) {
     res.json({
       result: "error",
       error: {
@@ -49,4 +18,33 @@ exports.login = async (req, res) => {
       },
     });
   }
-};
+
+  const { name, email } = verifiedToken;
+  const user = await User.findOne({ email }).lean();
+  let id = user._id;
+
+  if (!user) {
+    const newUser = await User.create({
+      name,
+      email,
+    });
+
+    id = newUser._id;
+  }
+
+  const accessToken = jwt.sign({ id }, secretKey, option);
+
+  if (accessToken) {
+    res.json({
+      result: "success",
+      token: accessToken,
+    });
+  }
+  res.json({
+    result: "error",
+    error: {
+      message: "Unauthorized",
+      status: 401,
+    },
+  });
+});
