1e97fb6fcb46805890d1341c2f42aa97c3c090d1
Wonhee Kang <eggfreitag@MacMorning.local>
2022-06-14 15:53:02 +0900

    chore: Change authorization logic to verify chrome-extension user

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -4,24 +4,34 @@ const User = require("../models/User");
 const catchAsync = require("../utils/catchAsync");
 
 const isLoggedIn = catchAsync(async (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
+  const verifier = req.headers?.authorization.split(" ")[0];
+  const token = req.headers?.authorization.split(" ")[1];
+  const email = req.headers?.email;
 
-  if (!token) {
-    return res.json({
-      result: "error",
-      error: {
-        message: "유효하지 않은 접근입니다.",
-        status: 400,
-      },
-    });
+  if (verifier === "Bearer" && token) {
+    const { id } = jwt.verify(token, process.env.SECRET_KEY);
+    const user = await User.findById(id).lean();
+
+    req.user = user;
+
+    return next();
   }
 
-  const { id } = jwt.verify(token, process.env.SECRET_KEY);
-  const user = await User.findById(id).lean();
+  if (verifier === "Extension" && token && jwt.verify(token, process.env.SECRET_KEY) === process.env.EXTENSION_KEY) {
+    const user = await User.findOne({ email }).lean();
 
-  req.user = user;
+    req.user = user;
 
-  return next();
+    return next();
+  }
+
+  return res.json({
+    result: "error",
+    error: {
+      message: "유효하지 않은 접근입니다.",
+      status: 400,
+    },
+  });
 });
 
 module.exports = isLoggedIn;
