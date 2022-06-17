fe10fe14d12f51d41e61133831ab49568aab8759
eggfreitag <93723399+eggfreitag@users.noreply.github.com>
2022-06-17 12:09:22 +0900

    refactor: Edit cors option to deploy (#25)
    Co-authored-by: Wonhee Kang <eggfreitag@MacMorning.local>

--- loader/cors.js
+++ loader/cors.js
@@ -1,15 +1,8 @@
 const cors = require("cors");
 
-const whiteList = ["http://localhost:3000", process.env.CHROME_EXTENSION];
 const corsOption = {
   credentials: true,
-  origin: (origin, callback) => {
-    if (whiteList.indexOf(origin) >= 0 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not Allowed"));
-    }
-  },
+  origin: "*",
 };
 
 module.exports = (app) => {
