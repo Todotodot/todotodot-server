879c366c8b8b8e1cdcd25715a1024f17e9b80e45
Wonhee Kang <eggfreitag@MacMorning.local>
2022-06-17 17:33:39 +0900

    fix: Fix aws bug

--- config/firebase.js
+++ config/firebase.js
@@ -1,8 +1,18 @@
 const admin = require("firebase-admin");
-const firebase = require("../firebase.json");
 
 const firebaseAdminConfig = {
-  credential: admin.credential.cert(firebase),
+  credential: admin.credential.cert({
+    type: "service_account",
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY,
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER,
+    client_x509_cert_url: process.env.CLIENT,
+  }),
 };
 
 const firebaseAdminApp = admin.initializeApp(firebaseAdminConfig);
